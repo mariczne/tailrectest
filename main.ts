4262a85c7de30a7c61c75fe7b76a2d08e9258c4a
mariczne <mariczne@gmail.com>
2023-11-04 07:33:22 +0100

    Use a linked list in the recursive solution

--- main.ts
+++ main.ts
@@ -1,24 +1,51 @@
 import { Dayjs } from "dayjs";
+import {
+  SinglyLinkedList as LinkedList,
+} from "linked-list-typed";
 
-function auxGenerateValidDatesForRange(startDate: Dayjs, endDate: Dayjs, step = 15, acc: Dayjs[]): Dayjs[] {
-  if (startDate.isAfter(endDate)) {
-    return acc;
+function auxGenerateValidDatesForRange(
+  nextDate: Dayjs,
+  endDate: Dayjs,
+  step = 15,
+  acc: LinkedList<Dayjs>
+): Dayjs[] {
+  if (nextDate.isAfter(endDate)) {
+    return acc.toArray();
   } else {
-    return auxGenerateValidDatesForRange(startDate.add(step, 'minute'), endDate, step, [...acc, startDate]);
+    acc.push(nextDate);
+    return auxGenerateValidDatesForRange(
+      nextDate.add(step, "minute"),
+      endDate,
+      step,
+      acc
+    );
   }
 }
 
-export function generateValidDatesForRangeRecursively(startDate: Dayjs, endDate: Dayjs, step = 15): Dayjs[] {
-  return auxGenerateValidDatesForRange(startDate, endDate, step, []);
+export function generateValidDatesForRangeRecursively(
+  startDate: Dayjs,
+  endDate: Dayjs,
+  step = 15
+): Dayjs[] {
+  return auxGenerateValidDatesForRange(
+    startDate,
+    endDate,
+    step,
+    new LinkedList()
+  );
 }
 
-export function generateValidDatesForRangeProcedurally(startDate: Dayjs, endDate: Dayjs, step = 15): Dayjs[] {
+export function generateValidDatesForRangeProcedurally(
+  startDate: Dayjs,
+  endDate: Dayjs,
+  step = 15
+): Dayjs[] {
   const dateList = [];
   let currentDate = startDate;
 
   while (currentDate <= endDate) {
     dateList.push(currentDate);
-    currentDate = currentDate.add(step, 'minute');
+    currentDate = currentDate.add(step, "minute");
   }
 
   return dateList;
